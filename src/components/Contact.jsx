7a829459eb2718becee8adf22a2dea9121cdd66a
Ryan Milligan <90968645+rmilligan006@users.noreply.github.com>
2022-06-08 08:06:03 -0400

    added required field to contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,18 +24,21 @@ const Contact = () => {
           type="text"
           placeholder="Name..."
           name="name"
+          aria-required
         />
         <input
           className="my-4 p-2 bg-[#ccd6f6]"
           type="text"
           placeholder="email..."
           name="email"
+          required
         />
         <textarea
           name="message"
           rows="10"
           className="bg-[#ccd6f6] p-2"
           placeholder="message..."
+          required
         ></textarea>
         <button className="text-white border-2 hover:bg-yellow-500 hover:border-yellow-500 ease-in-out duration-300 px-4 py-3 my-8 mx-auto flex items-center">
           Let's Collaborate
